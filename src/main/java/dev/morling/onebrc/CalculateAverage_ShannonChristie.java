2ccc7ce5f50438b11578b41541298c732da212dc
Shannon Christie <163936359+shannon-christie-nz@users.noreply.github.com>
2024-05-01 15:23:35 +1200

    Reduce buffer size, reduce timeouts.

--- src/main/java/dev/morling/onebrc/CalculateAverage_ShannonChristie.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_ShannonChristie.java
@@ -31,6 +31,7 @@ import java.util.concurrent.TimeUnit;
  * 200c564dc56729dfb7671a2aa53bd287ed51eb6b - 1r~15w. "" "" "". Took ~47 seconds.
  * 4ff30825f45578b171d5cd56ce886e914c03e323 - competition 1r~7w. "". Took ~43 seconds. - No major delays from disk IO.
  * 93caf58f7102f0e882d7f7556c440d3bf6e54944 - "". "". Reduce timeouts. Took ~36 seconds.
+ *
  * */
 public class CalculateAverage_ShannonChristie {
     private static volatile boolean readerHasFinished = false;
@@ -40,9 +41,9 @@ public class CalculateAverage_ShannonChristie {
     /////////////////////
     /// Configuration ///
     /////////////////////
-    private static final int BUFFER_SIZE = 100_000_000;
+    private static final int BUFFER_SIZE = 10_000_000;
     private static final int READER_TIMEOUT = 2;
-    private static final int WORKER_TIMEOUT = 2;
+    private static final int WORKER_TIMEOUT = 1;
 
     //////////////////////////
     /// Auto-configuration ///
