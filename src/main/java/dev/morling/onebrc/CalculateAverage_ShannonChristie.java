6e6cd34e4d7840a4eb2198c7420ef824f9ee14ea
Shannon Christie <163936359+shannon-christie-nz@users.noreply.github.com>
2024-05-03 10:17:17 +1200

    Add latest run

--- src/main/java/dev/morling/onebrc/CalculateAverage_ShannonChristie.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_ShannonChristie.java
@@ -43,7 +43,7 @@ import java.util.concurrent.TimeUnit;
  * e27e1a2f93bc4e10325b34587e7cea14fc10621d - "". Custom rolled name parsing without decoding. Took ~15 seconds.
  * 96dcc26897a0a771afd369b493074bb1f4dbcdb4 - 1r15w. "". Took ~12 seconds.
  * c235e14351eb2d8f701dbf208e89fe50c4ecca14 - "". Disable most logging. Took ~12 seconds.
- *
+ * 192bd37501fc9067b8bd7df9fd7667af8c7daa39 - "". Replace computeIfAbsent with manual map population. Took ~11 seconds (10.5~)
  * */
 public class CalculateAverage_ShannonChristie {
     private static volatile boolean readerHasFinished = false;
